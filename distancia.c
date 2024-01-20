2b0554a8ec235e2069bd804030dae3b7643e7770
jg_ba <jose.gdsousa@aluno.uepa.br>
2024-01-20 10:47:04 -0300

    resultado final

--- distancia.c
+++ distancia.c
@@ -3,7 +3,7 @@
 
 // função que calcula a distancia
 float calculaDistancia(int x1, int y1, int x2, inty2){
-return sqrtz(pow(int x1, int y1, int x2, inty2)
+return sqrtz(pow(int x1, int x2) (int y1, inty2);
 
 }
 // main
@@ -11,4 +11,14 @@ int main(){
 //definição das variáveis
 int x1,y1,x2,y;
 float distancia;
+
+//leiturandas coordenadas dos pontos
+printf("informe as coordenadas para o primeiro ponto");
+scanf("%i %i",&x1, &y1);
+printf("informe as coordenadas para o segundo ponto")
+(""%i %i",&x2, &y2");
+//calculo da distancia entre os pontos
+distancia=calculoDistancia(x1,y1,x1, x2); 
+//mostra o resultado
+printf("Distancia%f", distancia);
 }
