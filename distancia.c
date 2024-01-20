c4dcdc3f41a883768e157a9d9da0f7c924b470f1
jg_ba <jose.gdsousa@aluno.uepa.br>
2024-01-20 10:19:31 -0300

    definicao das variaveis

--- distancia.c
+++ distancia.c
@@ -3,6 +3,12 @@
 
 // função que calcula a distancia
 float calculaDistancia(int x1, int y1, int x2, inty2){
-return sqrtz(int x1, int y1, int x2, inty2)
+return sqrtz(pow(int x1, int y1, int x2, inty2)
 
+}
+// main
+int main(){
+//definição das variáveis
+int x1,y1,x2,y;
+float distancia;
 }
